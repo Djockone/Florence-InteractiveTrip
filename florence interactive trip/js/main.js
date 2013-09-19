d34d7d8721255d8fd146d2cf6f2cb24fcfabe6b5
Djockone <timothy.potter@s203-02.local>
2013-09-19 15:32:30 +0200

    fonction temporelle
    Permet de gerer l'espace temps

--- florence interactive trip/js/main.js
+++ florence interactive trip/js/main.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
 
-	var horloge= date().getHours();
+	
+setInterval(function() {
+ 	var opacite;
+	var date = new Date();
+	var sec=date.getSeconds();
+	$('h1.hour').html(date);
+	//var opacite = -0.0166666*sec+1; test seconde
+	sec=13;
+	if (sec >= 12) {
+		opacite = (-1/12)*sec+2;
+	}else{
 
-	$('h1.hour').append(horloge);
+		opacite = (1/-12)*sec+1;
+	};
+	
+	$('h1.op').html(opacite);
+ 	$('#noir').css({opacity:opacite});
+
+/*if(sec > 20 && sec < 22){
+	alert('super');
+}*/
+
+
+}, 1);
+
+
+	
 });
